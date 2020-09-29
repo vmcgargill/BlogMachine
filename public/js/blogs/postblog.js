0c589591e68bd5d5f3806c81a9824f11c7e2786f
Vincent McGargill <vincentmcgargill@gmail.com>
2020-09-29 17:27:33 -0600

    Last minute change, fixing global varialbles for postblog.js file so it can adapt to edit blog as well.

--- public/js/blogs/postblog.js
+++ public/js/blogs/postblog.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
     const MoodDropdown = $("#mood");
+    const url = window.location.search;
+    let Id;
+    const updating = false;
 
     MoodDropdown.change(function() {
         if (MoodDropdown.val() === "other") {
@@ -14,6 +17,13 @@ $(document).ready(function() {
         // This does nothing yet!
     }
 
+    // Check if this is a blog being updated
+    if (url.indexOf("?blog_id=") !== -1) {
+        Id = url.split("=")[1];
+        updating = true;
+        renderBloginputs(Id)
+    }
+
     function handleLoginErr() {
         $("#alert .msg").text("Error: It looks like something went wrong. Please try again.");
         $("#alert").fadeIn(500);
@@ -22,10 +32,7 @@ $(document).ready(function() {
     // Submit blog
     const submitBlog = (event) => {
         event.preventDefault();
-        let Id;
         let mood = $("#mood").val();
-        const url = window.location.search;
-        const updating = false;
         const title = $("#title").val();
         const body = $("#body").val();
         const category = $("#category").val();
@@ -34,12 +41,6 @@ $(document).ready(function() {
             mood = $("#moodInput").val();
         }
     
-        // Check if this is a blog being updated
-        if (url.indexOf("?blog_id=") !== -1) {
-            Id = url.split("=")[1];
-            updating = true;
-            renderBloginputs(Id)
-        }
 
         if (body.trim() === "" || title.trim() === "") {
             $("#alert .msg").text("Title and body field cannot be left empty");
