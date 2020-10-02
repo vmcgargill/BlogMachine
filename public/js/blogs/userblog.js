108bb79f30ee1f786de2ec46b6d0e70985a7e14b
Maynard Peralta <maynperalta@gmail.com>
2020-10-01 19:52:22 -0600

    Finished delete-blogs

--- public/js/blogs/userblog.js
+++ public/js/blogs/userblog.js
@@ -2,3 +2,16 @@ $("#edit-blog").on("click", function() {
     let id = $(this).val();
     window.location.href = "/editBlog/?blog_id=" + id;
 })
+
+$("#delete-blog").on("click", function() {
+    let id = $(this).val();
+    let deleteBlog = confirm("Are you sure you want to delete this blog?");
+    if (deleteBlog === true) {
+        $.ajax({
+            method: "DELETE",
+            url: "/api/blogs/" + id
+        }).then(function() {
+            window.location.href = "/"
+        })
+    }
+})
