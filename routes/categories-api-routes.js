8affd82d75ba69a817cd7aecf6b139d941502724
Maynard Peralta <maynperalta@gmail.com>
2020-10-03 13:11:24 -0600

    added create category

--- routes/categories-api-routes.js
+++ routes/categories-api-routes.js
@@ -3,18 +3,20 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 const passport = require("passport");
 
 module.exports = function(app) {
-    app.get("/api/categories", function(req, res) {
-        db.Category.findAll({
-          }).then(function(category) {
-            let categoryArray = new Array();
-            category.forEach((category) => {
-              categoryArray.push({
-                
-                name: category.name
-              });
-            });
-      
-            res.render("partials/viewcategory", { blog: categoryArray });
-          });
-        });
-    }
+    app.post("/api/categories", isAuthenticated, function(req, res) {
+        db.Category.findOne({where: {name: req.body.name}}).then(function(Category) {
+            if (Category === null) {
+                db.Category.create({name: req.body.name}).then(function(data) {
+                    res.json(data)
+                })
+            } else {
+                res.json({})
+            }
+        })
+    })
+    app.delete("/api/categories/:id", isAuthenticated, function(req, res) {
+        db.Category.destroy({where: {id: req.params.id}}).then(function() {
+            res.json({})
+        })
+    }) 
+}
