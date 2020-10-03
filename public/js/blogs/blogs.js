64033a74b20a6f688c5ea0a8afe2a225a67893c6
Vincent McGargill <vincentmcgargill@gmail.com>
2020-10-03 16:22:50 -0600

    Fixed blog search feature

--- public/js/blogs/blogs.js
+++ public/js/blogs/blogs.js
@@ -33,24 +33,11 @@ DropDowns.change(function() {
 });
 
 $("#SearchFilteresBlogs").keyup(function() {
-    let SearchFilteresBlogs = document.getElementById("SearchFilteresBlogs").value.toUpperCase();
-    let BlogList = document.getElementById("BlogList").getElementsByTagName("li");
+    let SearchFilteresBlogs = $("#SearchFilteresBlogs").val().toUpperCase();
+    let BlogList = document.getElementsByClassName("blogSearch");
+    console.log(BlogList)
     for (let i = 0; i < BlogList.length; i++) {
-        let BlogTitles = BlogList[i].getElementsByTagName("a")[0];
-        let BlogDescriptions = BlogList[i].getElementsByTagName("a")[1];
-        let BlogAuthors = BlogList[i].getElementsByTagName("a")[2];
-        let BlogCategories = BlogList[i].getElementsByTagName("a")[3];
-        let BlogMood = BlogList[i].getElementsByTagName("a")[4];
-        let titleText = BlogTitles.textContent || BlogTitles.innerText;
-        let descriptionText = BlogDescriptions.textContent || BlogDescriptions.innerText;
-        let authorText = BlogAuthors.textContent || BlogAuthors.innerText;
-        let categoryText = BlogCategories.textContent || BlogCategories.innerText;
-        let moodText = BlogMood.textContent || BlogMood.innerText;
-        if (titleText.toUpperCase().indexOf(SearchFilteresBlogs) > -1 || 
-        descriptionText.toUpperCase().indexOf(SearchFilteresBlogs) > -1 || 
-        authorText.toUpperCase().indexOf(SearchFilteresBlogs) > -1 || 
-        categoryText.toUpperCase().indexOf(SearchFilteresBlogs) > -1 || 
-        moodText.toUpperCase().indexOf(SearchFilteresBlogs) > -1) {
+        if (BlogList[i].innerText.toUpperCase().indexOf(SearchFilteresBlogs) > -1) {
             BlogList[i].style.display = "";
         } else {
             BlogList[i].style.display = "none";
