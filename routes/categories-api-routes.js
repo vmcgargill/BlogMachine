665669384cbf217c958f5897afe458b3c2aac009
Maynard Peralta <maynperalta@gmail.com>
2020-10-03 10:32:15 -0600

    added api/categories code

--- routes/categories-api-routes.js
+++ routes/categories-api-routes.js
@@ -1,6 +1,20 @@
 const db = require("../models");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
+const passport = require("passport");
 
 module.exports = function(app) {
-    
-};
+    app.get("/api/categories", function(req, res) {
+        db.Category.findAll({
+          }).then(function(category) {
+            let categoryArray = new Array();
+            category.forEach((category) => {
+              categoryArray.push({
+                
+                category: category.Category.name
+              });
+            });
+      
+            res.render("partials/filtercategories", { layout: false, blog: categoryArray });
+          });
+        });
+    }
